789cfaea308423a74ea95c7f1f9d144af3801053
WoojinKim <dnwls488@naver.com>
2022-06-27 09:50:11 +0900

    member test

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,52 @@
+package study.querydsl.entity;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @PersistenceContext EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = Member.builder().username("member1").age(10).team(teamA).build();
+        Member member2 = Member.builder().username("member2").age(20).team(teamA).build();
+
+        Member member3 = Member.builder().username("member3").age(30).team(teamB).build();
+        Member member4 = Member.builder().username("member4").age(40).team(teamB).build();
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush();
+        em.clear();
+
+        // 확인
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        assertThat(members.get(0).getUsername()).isEqualTo(member1.getUsername());
+
+    }
+
+}
